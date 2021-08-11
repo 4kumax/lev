9ffd4f329dfc54048489d1e8db7e116958f9d5e8
4kumax <businessmax@mail.ru>
2021-08-11 15:58:20 +0300

    first awesome commit

---
+++ levgit/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from levgit.users.models import User
+from levgit.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
